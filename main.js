d57c18855e134a1f1306b2824267b53ddb885e17
Paul2D <paul.maxineanu@gmail.com>
2022-11-29 17:19:15 +0200

    add utils functions

--- main.js
+++ main.js
@@ -7,27 +7,6 @@ import Element from './src/element.js';
 //create list of elements with class 'tasks' and add them to the list
 let app = new Element('#app');
 
-// let tasksList = new Element('ul', true);
-
-// let task1 = new Element('li', true, 'task1');
-// task1.addStyle('color', 'red');
-
-// let task2 = new Element('li', true, 'task2');
-// task2.addStyle('color', 'green');
-
-// let task3 = new Element('li', true, 'task3');
-// task3.addStyle('color', 'blue');
-
-
-
-// let tasks = [task1, task2, task3];
-
-// console.log(task2);
-
-// tasksList.addElements(tasks);
-
-// app.addElement(tasksList);
-
 let newTask = new Element('#newTask');
 let tasksList = new Element('#tasks');
 
@@ -45,4 +24,6 @@ newTask.registerEvent('keyup', (e) => {
 
 app.addElement(tasksList);
 
+Utils.addClass(app.element, 'app');
 
+console.log(Utils.addDaysToDate('2022-11-29', 3));
--- utils.js
+++ utils.js
@@ -2,4 +2,56 @@ export default class Utils {
   static logText(text) {
     console.log(text);
   }
+
+  /*
+  * Adds a class to an HTML element.
+  * Use Element.classList and DOMTokenList.add() to add the specified class to the element.
+  */ 
+
+  static addClass = (element, className) => {
+    element.classList.add(className);
+  };
+
+  /*
+  * Joins all given URL segments together, then normalizes the resulting URL.
+  * Use Array.prototype.join() to combine URL segments.
+  * Use a series of String.prototype.replace() calls with various regular expressions to normalize the resulting URL 
+  * (remove double slashes, add proper slashes for protocol,
+  *  remove slashes before parameters,
+  *  combine parameters with '&' and normalize first parameter delimiter).
+  */ 
+
+  static urlJoin = (...args) =>
+                  args
+                    .join('/')
+                    .replace(/[\/]+/g, '/')
+                    .replace(/^(.+):\//, '$1://')
+                    .replace(/^file:/, 'file:/')
+                    .replace(/\/(\?|&|#[^!])/g, '$1')
+                    .replace(/\?/g, '&')
+                    .replace('&', '?');
+
+  /*
+  * Creates an array of partial sums.
+  * Use Array.prototype.reduce(), initialized with an empty array accumulator to iterate over nums.
+  * Use Array.prototype.slice() to get the previous partial sum or 0 and add the current element to it.
+  * Use the spread operator (...) to add the new partial sum to the accumulator array containing the previous sums.
+  */
+ 
+  static accumulate = (...nums) => nums.reduce((acc, n) => [...acc, n + (acc.slice(-1)[0] || 0)], []);
+
+  /*
+  * Calculates the date of n days from the given date, returning its string representation.
+  * Use the Date constructor to create a Date object from the first argument.
+  * Use Date.prototype.getDate() and Date.prototype.setDate() to add n days to the given date.
+  * Use Date.prototype.toISOString() to return a string in yyyy-mm-dd format.
+  */
+  
+  static addDaysToDate = (date, n) => {
+    const d = new Date(date);
+    d.setDate(d.getDate() + n);
+    return d.toISOString().split('T')[0];
+  };  
+
+
 }
